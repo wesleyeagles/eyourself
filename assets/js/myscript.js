2e6e55c3c7511fb508059d50024d9228510bf832
wesleyeagles <84718917+wesleyeagles@users.noreply.github.com>
2021-12-10 08:32:40 -0300

    Add files via upload

---
+++ assets/js/myscript.js
@@ -0,0 +1,261 @@
+function applyColor () {
+    document.getElementsByClassName('name-1')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-1')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-1')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-1')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-1')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor () {
+    document.getElementsByClassName('name-1')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-1')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-1')[0].classList.add('highlight');
+    document.getElementsByClassName('product-1')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-1')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor2 () {
+    document.getElementsByClassName('name-2')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-2')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-2')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-2')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-2')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor2 () {
+    document.getElementsByClassName('name-2')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-2')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-2')[0].classList.add('highlight');
+    document.getElementsByClassName('product-2')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-2')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor3 () {
+    document.getElementsByClassName('name-3')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-3')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-3')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-3')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-3')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor3 () {
+    document.getElementsByClassName('name-3')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-3')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-3')[0].classList.add('highlight');
+    document.getElementsByClassName('product-3')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-3')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor4 () {
+    document.getElementsByClassName('name-4')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-4')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-4')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-4')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-4')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor4 () {
+    document.getElementsByClassName('name-4')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-4')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-4')[0].classList.add('highlight');
+    document.getElementsByClassName('product-4')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-4')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor5 () {
+    document.getElementsByClassName('name-5')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-5')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-5')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-5')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-5')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor5 () {
+    document.getElementsByClassName('name-5')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-5')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-5')[0].classList.add('highlight');
+    document.getElementsByClassName('product-5')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-5')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor6 () {
+    document.getElementsByClassName('name-6')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-6')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-6')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-6')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-6')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor6 () {
+    document.getElementsByClassName('name-6')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-6')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-6')[0].classList.add('highlight');
+    document.getElementsByClassName('product-6')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-6')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor7 () {
+    document.getElementsByClassName('name-7')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-7')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-7')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-7')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-7')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor7 () {
+    document.getElementsByClassName('name-7')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-7')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-7')[0].classList.add('highlight');
+    document.getElementsByClassName('product-7')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-7')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor8 () {
+    document.getElementsByClassName('name-8')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-8')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-8')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-8')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-8')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor8 () {
+    document.getElementsByClassName('name-8')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-8')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-8')[0].classList.add('highlight');
+    document.getElementsByClassName('product-8')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-8')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor9 () {
+    document.getElementsByClassName('name-9')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-9')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-9')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-9')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-9')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor9 () {
+    document.getElementsByClassName('name-9')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-9')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-9')[0].classList.add('highlight');
+    document.getElementsByClassName('product-9')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-9')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor10 () {
+    document.getElementsByClassName('name-10')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-10')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-10')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-10')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-10')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor10 () {
+    document.getElementsByClassName('name-10')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-10')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-10')[0].classList.add('highlight');
+    document.getElementsByClassName('product-10')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-10')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor11 () {
+    document.getElementsByClassName('name-11')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-11')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-11')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-11')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-11')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor11 () {
+    document.getElementsByClassName('name-11')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-11')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-11')[0].classList.add('highlight');
+    document.getElementsByClassName('product-11')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-11')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor12 () {
+    document.getElementsByClassName('name-12')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-12')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-12')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-12')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-12')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor12 () {
+    document.getElementsByClassName('name-12')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-12')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-12')[0].classList.add('highlight');
+    document.getElementsByClassName('product-12')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-12')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor13 () {
+    document.getElementsByClassName('name-13')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-13')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-13')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-13')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-13')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor13 () {
+    document.getElementsByClassName('name-13')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-13')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-13')[0].classList.add('highlight');
+    document.getElementsByClassName('product-13')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-13')[0].classList.add('degrade-color');
+    
+}
+
+function applyColor14 () {
+    document.getElementsByClassName('name-14')[0].classList.toggle('highlight');
+    document.getElementsByClassName('checkbox-14')[0].classList.toggle('highlight');
+    document.getElementsByClassName('quantity-14')[0].classList.toggle('highlight');
+    document.getElementsByClassName('product-14')[0].classList.toggle('box-shadow');
+    document.getElementsByClassName('image-14')[0].classList.toggle('degrade-color');
+    
+}
+
+function applyCColor14 () {
+    document.getElementsByClassName('name-14')[0].classList.add('highlight');
+    document.getElementsByClassName('checkbox-14')[0].classList.add('highlight');
+    document.getElementsByClassName('quantity-14')[0].classList.add('highlight');
+    document.getElementsByClassName('product-14')[0].classList.add('box-shadow');
+    document.getElementsByClassName('image-14')[0].classList.add('degrade-color');
+    
+}
+
+
+
+
+
+
+
+
+
+
